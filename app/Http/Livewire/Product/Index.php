ba17a5bbee83d57f00b595591b4f83b0ca4ad3dc
rzhasibuan <rzhasibuan@gmail.com>
2021-06-25 21:45:07 +0700

    add search

--- app/Http/Livewire/Product/Index.php
+++ app/Http/Livewire/Product/Index.php
@@ -7,13 +7,33 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    // public variabel
     public $paginate = 10;
+    public $search ;
+    
+    // this method for the make pretty url from search using except 
+    public $updatesQueryString = [
+        ['search' => ['except' => '']] 
+    ];
 
-    public function render()
+    // method mount mirip seperti counstract  
+    public function mount()
     {
-        
+        $this->search = request()->query('search',$this->search);
+    }
+    
+    public function render()
+    // if search is null show tables with paginate 
+    {
         return view('livewire.product.index', [
-            'products' => Product::latest()->paginate($this->paginate)
+            'products' => $this->search === null ?
+                Product::latest()->paginate($this->paginate) : 
+            //  if search have a value show data
+                Product::latest()->where('title', 'like', '%' .$this->search. '%')
+                ->paginate($this->paginate)
         ]);
     }
+
+    
+    
 }
